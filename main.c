9623765eee5bbcfbc0a77460dfb56af6a9676de5
yebin048 <lyna0408@naver.com>
2025-11-13 15:44:16 +0900

    6번 실습

--- main.c
+++ main.c
@@ -5,20 +5,15 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i;
-	int grade[5];
-	int sum=0;
+	int i=100;
+	int *p=&i;
+	int **q= &p;
 	
-	for (i=0; i<5; i++){
-		printf("grade[%i]=", i);
-		scanf("%d", &grade[i]);
-	}
+	*p = 200;
+	printf("i=%d, *p=%d, **q=%d\n", i, *p, **q);
 	
-	for (i=0; i<5; i++){
-		printf("grade[%i] = %i\n", i, *(grade+i));
-		sum=sum + *(grade+i);
-	}
-	printf("Average: %i\n", sum/5);
+	**q = 300;
+	printf("i=%d, *p=%d, **q=%d\n", i, *p, **q);
 	
 	system("PAUSE");
 	return 0;
